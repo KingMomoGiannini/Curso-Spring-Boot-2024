bfafd7d90da8ae488634903cd7e04711ef392285
Sebastian <sebastiangiannini95@gmail.com>
2024-05-09 16:09:05 -0300

    Ejercicio 2 GetMapping

--- Ejercicios/GetMapping/ej2 - Hello Bye/hellobye/src/main/java/com/ejercicios/hellobye/controller/HelloByeController.java
+++ Ejercicios/GetMapping/ej2 - Hello Bye/hellobye/src/main/java/com/ejercicios/hellobye/controller/HelloByeController.java
@@ -5,6 +5,7 @@
 package com.ejercicios.hellobye.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,13 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloByeController {
     
-    @GetMapping ("/hello")
-    public String sayHello(){
-        return "Hola Mundo";
+    @GetMapping ("/hello/{nombre}")
+    public String sayHello(@PathVariable String nombre){
+        return "Hola Mundo " + nombre;
     }
     
-    @GetMapping("/bye")
-    public String sayBye(){
-        return "Adios Mundo";
+    @GetMapping ("hello/{nombre}/{edad}/{profesion}")
+    public String sayHelloCompleto(@PathVariable String nombre,
+                                   @PathVariable int edad,
+                                   @PathVariable String profesion){
+        return "Hola mundo"
+                + "Tu nombre es: " + nombre + 
+                "Tu edad es: " + edad + 
+                "Profesion: " + profesion;
+    }
+    
+    @GetMapping("/bye/{nombre}")
+    public String sayBye(@PathVariable String nombre){
+        return "Adios Mundo " + nombre ;
     }
 }
